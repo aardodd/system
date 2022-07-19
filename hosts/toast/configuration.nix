e16dd521be761dcbc31af203216fa1b52e1cc653
Aaron Dodd <aardodd@users.noreply.github.com>
2022-07-19 21:52:55 +0100

    mend

--- hosts/toast/configuration.nix
+++ hosts/toast/configuration.nix
@@ -1,10 +1,12 @@
 { config, pkgs, modulesPath, ... }: {
   imports = [
     "${modulesPath}/installer/scan/not-detected.nix"
-    ../../profiles/boot/grub.nix
+    (import ../../profiles/boot/grub.nix { device = "/dev/sda"; })
     ../../profiles/display/x.nix
     ../../users/aaron
     ../../users/andreea
     ../../users/guest
   ];
+
+  networking.hostname = "toast";
 }
