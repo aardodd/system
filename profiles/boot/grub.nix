6ff387ecee16f25a3085d2a8afa40939c2f46c9d
Aaron Dodd <aardodd@users.noreply.github.com>
2022-07-20 19:42:27 +0100

    Add plymouth splash

--- profiles/boot/grub.nix
+++ profiles/boot/grub.nix
@@ -1,18 +1,24 @@
 { device, ... }: {
-  boot.loader = {
-    grub = {
-      enable = true;
-      version = 2;
-      inherit device;
-      memtest86.enable = true;
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        version = 2;
+        inherit device;
+        memtest86.enable = true;
+      };
+
+      grub2-theme = {
+        enable = true;
+        icon = "white";
+        theme = "whitesur";
+        screen = "1080p";
+        footer = true;
+      };
     };
 
-    grub2-theme = {
+    plymouth = {
       enable = true;
-      icon = "white";
-      theme = "whitesur";
-      screen = "1080p";
-      footer = true;
     };
   };
 }
