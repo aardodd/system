8e1e256dacbaa92a4985a5f5560a00407f34dc5e
Aaron Dodd <aardodd@users.noreply.github.com>
2022-07-21 10:12:46 +0100

    Refactor gnome desktop

--- suites/desktop.nix
+++ suites/desktop.nix
@@ -3,7 +3,7 @@
     ./base.nix
     (import ../profiles/boot/grub.nix { inherit config device; })
     ../profiles/audio/pipewire.nix
-    ../profiles/display/wayland.nix
+    ../profiles/desktop/gnome.nix
     ../profiles/programs/shells/zsh.nix
   ];
 }
