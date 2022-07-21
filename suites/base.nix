32bcb7df232994f8dd7cd7151a72c936501feb6c
Aaron Dodd <=>
2022-07-21 08:09:31 +0100

    Update audio and nix-settings

--- suites/base.nix
+++ suites/base.nix
@@ -3,8 +3,18 @@
   system.stateVersion = "22.05";
 
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixUnstable;
     extraOptions = "experimental-features = nix-command flakes";
+
+    settings = {
+      auto-optimise-store = true;
+    };
+
+    gc = {
+      automatic = true;
+      dates = "23:00";
+      options = "--delete-older-than 30d";
+    };
   };
 
   environment.systemPackages = with pkgs; [
--- suites/desktop.nix
+++ suites/desktop.nix
@@ -2,6 +2,7 @@
   imports = [
     ./base.nix
     (import ../profiles/boot/grub.nix { inherit config device; })
+    ../profiles/audio/pipewire.nix
     ../profiles/display/wayland.nix
     ../profiles/programs/shells/zsh.nix
   ];
