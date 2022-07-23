07f41286b7424079b33578def25a3c090b866fc4
Aaron Dodd <=>
2022-07-23 13:18:46 +0100

    Add xfce and plasma with networking

---
+++ profiles/desktop/xfce.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }: {
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.lightdm.enable = true;
+      desktopManager.xfce.enable = true;
+    };
+  };
+}
---
+++ suites/plasma-desktop.nix
@@ -0,0 +1,12 @@
+{ config, device, ... }: {
+  imports = [
+    ./base.nix
+    (import ../profiles/boot/grub.nix { inherit config device; })
+    ../profiles/audio/pipewire.nix
+    ../profiles/fonts/common.nix
+    ../profiles/desktop/plasma.nix
+    ../profiles/programs/shells/zsh.nix
+  ];
+
+  networking.networkmanager.enable = true;
+}
